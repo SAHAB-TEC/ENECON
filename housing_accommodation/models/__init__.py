86c4fcec46a9617bcbfbc665693a919510032a51
SAHAB <sahab.for.it.solutions@gmail.com>
2026-09-02 20:50:17 +0200

    Remove old housing_accommodation module

--- housing_accommodation/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import housing_unit_type
-from . import housing_property_unit
-from . import housing_property_content
-from . import housing_guest_allocation
-from . import housing_guest_belonging
