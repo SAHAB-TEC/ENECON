5780a774262396e2d51f73fccff44151dfbce7d6
ragabmahmoud1 <ragabdeaf93@outlook.com>
2026-09-10 11:43:17 +0300

    Update author information in HR Extended Features manifest from 'Marwah Adel' to 'RGB'

--- hr_extended_features/__manifest__.py
+++ hr_extended_features/__manifest__.py
@@ -2,7 +2,7 @@
     'name': 'HR Extended Features',
     'version': '18.0.1.0',
     'category': 'Human Resources',
-    'author': 'Marwah Adel',
+    'author': 'RGB',
     'license': 'LGPL-3',
     'summary': 'Unpaid Leave, Appraisal Updates, Multi Shifts',
     'depends': ['hr', 'hr_attendance', 'hr_payroll', 'hr_contract', 'hr_appraisal','hr_holidays','rm_hr_attendance_sheet','planning','spreadsheet'],
